c035e90a02ae890210e1a1a404a4a61fb55310e8
whois1t <reyyanahmed77@gmail.com>
2024-07-25 20:15:55 +0530

    update protected directory

--- config/site.config.js
+++ config/site.config.js
@@ -42,7 +42,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/Personal'],
+  protectedRoutes: ['/Personal','/EC Council(Certified Network Defender)'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'mailto:reyyan.ahmed.cs@gmail.com',
